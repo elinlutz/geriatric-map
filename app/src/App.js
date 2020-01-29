24d6edb685a9240268deed200d4fe0db88cb870e
Elin Lütz <lutz.elin@gmail.com>
2020-01-29 22:19:43 +0100

    Add list of units

--- app/src/App.js
+++ app/src/App.js
@@ -3,16 +3,19 @@ import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps
 import logo from './logo.svg';
 import './App.css';
 
+var units = [
+  { id: 1, name: 'Magdalenagården', latitude: 59.3150, longitude: 18.094620},
+  { id: 2, name: 'Katarinagården', latitude: 59.3120, longitude: 18.09930},
+  { id: 3, name: 'Nytorggården', latitude: 59.3140, longitude: 18.096920},
+  { id: 4, name: 'Attendo Lignagatan', latitude: 59.3130, longitude:18.094620}
+];
+
 const options = {
   zoomControlOptions: {
     // position: google.maps.ControlPosition.RIGHT_CENTER // ,
     // ...otherOptions
   },
 }
-
-// const infoOptions = {
-//   content: "hejhejhej"
-// }
 
 function CantLoad() {
   return <p>Can't load</p>
@@ -28,40 +31,40 @@ const center = {
   lng: 18.091620
 }
 
-const position = {
-  lat: 59.312875,
-  lng: 18.091620
-}
-
 const divStyle = {
   background: `white`,
-  border: `1px solid #ccc`,
+  border: `0px solid #ccc`,
   padding: 15
 }
 
 const onLoad = marker => {
-  console.log('marker: ', marker)
+  //console.log('marker: ', marker)
+}
+
+function Markers() {
+  return units.map(unit => {
+    return (
+    <Marker
+      onLoad={onLoad}
+      key={unit.id}
+      position={{lat: unit.latitude, lng: unit.longitude}}
+      visible={true}
+      label={{ text: unit.name, fontSize: "12px", fontWeight: "bold" }}
+      title={unit.name}
+    />)
+  })
 }
 
 function MyComponent() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyDXvhN_6f1TXjc-6YlFO_Exo5hN98yh23o",
-    language: "swedish",
+    language: "sv",
   })
   const [showInfo, setShowInfo] = useState(false);
-
 
   const onClick = () => setShowInfo(true)
 
   const renderMap = () => {
-    // wrapping to a function is useful in case you want to access `window.google`
-    // to eg. setup options or create latLng object, it won't be available otherwise
-    // feel free to render directly if you don't need that
-    // const onLoad = React.useCallback(
-    //   function onLoad (mapInstance) {
-    //     // do something with map Instance
-    //   }
-    // )
     return <GoogleMap
       options={options}
       id="marker-example"
@@ -69,22 +72,7 @@ function MyComponent() {
       center={center}
       zoom={15}
     >
-    <Marker
-      onLoad={onLoad}
-      position={position}
-      visible={true}
-      onClick={onClick}
-    >
-    {showInfo ? 
-    <InfoWindow
-      position={position}
-      onCloseClick={() => setShowInfo(false)}>
-      <div style={divStyle}>
-        <h1>Hem</h1>
-      </div>
-      </InfoWindow> : null}
-    </Marker>
-
+    <Markers/>
   </GoogleMap>
   }
 
@@ -95,10 +83,32 @@ function MyComponent() {
   return isLoaded ? renderMap() : <CantLoad />
 }
 
+    {/* <Marker
+      onLoad={onLoad}
+      position={position}
+      visible={true}
+      onClick={onClick}
+    >
+
+    {showInfo ? 
+    <InfoWindow
+      position={position}
+      onCloseClick={() => setShowInfo(false)}>
+      <div style={divStyle}>
+        <h1>
+        {position.lat}
+        {' '}
+        {position.lng}
+        </h1>
+      </div>
+      </InfoWindow> : null}
+    </Marker> */}
+
 function Test() {
   return (
     <div className="App">
       <header className="App-header">
+        Hallå
         <MyComponent></MyComponent>
       </header>
     </div>
